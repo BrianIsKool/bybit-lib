c0f082501daf1822f0dda63208689d5b8dc5822e
BrianIsKool <blistka@gmail.com>
2025-04-02 18:39:52 +0200

    reconnect

--- utils/websocket_utils.py
+++ utils/websocket_utils.py
@@ -28,61 +28,63 @@ class websocket_utils:
 
         
     async def subscribe(self, queue, is_hidden=False, subscribe_message=''):
-        """_summary_
-
-        Args:
-            queue (_type_): _description_
-        """
-        if is_hidden == True:
-            self.URL = self.HIDDEN_URL
-            self.topics = self.hidden_topics
-            
-        async with websockets.connect(self.URL) as websocket:
-            if is_hidden == True:
-                ping_task = asyncio.create_task(self.ws_ping(interval=15, websocket=websocket))
-                # extra_params = {self.topics[0].split(':')[1].split['='][0]: self.topics[0].split(':')[1].split['='][1]}
-                # subscribe_message = {"topic":self.topics[0].split('.')[0],
-                                    #  "symbol":self.topics[0].split('.')[-1],
-                                    #  "limit": 0 if ,
-                                    #  "params":{"binary":False,"limit":1, extra_params[0]:extra_params[1]},
-                                    #  "event":"sub"}
-                if "mergedDepth" in self.topics[0]:
-                    subscribe_message = {"topic":self.topics[0].split('.')[0].split("_")[0], #mergedDepth_40.BTCUSDT:dumpScale=1
-                                        "symbol":self.topics[0].split('.')[-1].split(':')[0],
-                                        
-                                        "params":{"binary":False, "dumpScale": self.topics[0].split(':')[1].split('=')[1], "limit": int(self.topics[0].split('.')[0].split("_")[1]),},
-                                        "event":"sub"}
-                elif "kline" in self.topics[0]:
-                    subscribe_message = {"topic":self.topics[0].split('.')[0],"params":{"binary":False,"limit":1},"symbol":self.topics[0].split('.')[-1],"event":"sub"}
-                    
-            else:
-                subscribe_message = {
-                    "op": "subscribe",
-                    "args": self.topics
-                }
-
-            await websocket.send(json.dumps(subscribe_message))
-            print(f"subscribed channels: {self.topics}")
-            while True:
-                response = await websocket.recv()  # Получаем данные
+        """Подписка на WebSocket с автоматическим переподключением."""
+        
+        while True:
+            try:
+                if is_hidden:
+                    self.URL = self.HIDDEN_URL
+                    self.topics = self.hidden_topics
                 
-                if is_hidden == True:
-                    if isinstance(response, str):
-                        await queue.put(json.loads(response))
-                        # print("Received (string):", response)
-                    elif isinstance(response, bytes):
-                        # Если это байты — разжимаем gzip
-                        decompressed_data = gzip.decompress(response)
-                        decoded_text = decompressed_data.decode("utf-8")
-                        json_data = json.loads(decoded_text)
-                        await queue.put(json_data)
-                    
-                else:
-                    await queue.put(json.loads(response))
+                async with websockets.connect(self.URL) as websocket:
+                    if is_hidden:
+                        ping_task = asyncio.create_task(self.ws_ping(interval=15, websocket=websocket))
+                        
+                        if "mergedDepth" in self.topics[0]:
+                            subscribe_message = {
+                                "topic": self.topics[0].split('.')[0].split("_")[0], 
+                                "symbol": self.topics[0].split('.')[-1].split(':')[0],
+                                "params": {
+                                    "binary": False,
+                                    "dumpScale": self.topics[0].split(':')[1].split('=')[1],
+                                    "limit": int(self.topics[0].split('.')[0].split("_")[1]),
+                                },
+                                "event": "sub"
+                            }
+                        elif "kline" in self.topics[0]:
+                            subscribe_message = {
+                                "topic": self.topics[0].split('.')[0],
+                                "params": {"binary": False, "limit": 1},
+                                "symbol": self.topics[0].split('.')[-1],
+                                "event": "sub"
+                            }
+                    else:
+                        subscribe_message = {
+                            "op": "subscribe",
+                            "args": self.topics
+                        }
+
+                    await websocket.send(json.dumps(subscribe_message))
+                    print(f"Subscribed to channels: {self.topics}")
+
+                    while True:
+                        response = await websocket.recv() 
+                        
+                        if is_hidden:
+                            if isinstance(response, str):
+                                await queue.put(json.loads(response))
+                            elif isinstance(response, bytes):
+                                decompressed_data = gzip.decompress(response)
+                                decoded_text = decompressed_data.decode("utf-8")
+                                json_data = json.loads(decoded_text)
+                                await queue.put(json_data)
+                        else:
+                            await queue.put(json.loads(response))
+
+            except (websockets.ConnectionClosed, asyncio.TimeoutError, OSError) as e:
+                print(f"WebSocket connection lost: {e}, attempting to reconnect...")
+                await asyncio.sleep(5)
 
         pass
     
-    async def resubscribe(self):
-        pass
-        
     
